b97a8352ab3bc2fb9debb35960b3421a6960e7a3
vshpa <shpad1997@gmail.com>
2019-07-20 18:39:31 +0300

    create main page and game page

---
+++ inchkiev/src/views/Game.vue
@@ -0,0 +1,108 @@
+<template>
+    <div class="game">
+      <div class="game__sidebar">
+        <div class="game__menu">
+          <div class="game__menu-item"> <img src="../assets/icons/home.svg"> </div>
+          <div class="game__menu-item"> <img src="../assets/icons/home.svg"> </div>
+        </div>
+        <div class="game__params">
+          <p class="game__params-title">Params:</p>
+          <div class="game__params-item  mr-10">
+            <img src="../assets/icons/sad_face.svg"> <span class="param__count">2</span>
+          </div>
+          <div class="game__params-item">
+            <img src="../assets/icons/happy_face.svg">  <span class="param__count">3</span>
+          </div>
+          <div class="game__params-item">
+            <img src="../assets/icons/heart.svg">  <span class="param__count">4</span>
+          </div>
+        </div>
+        <div class="game__queue">
+          Осталось в очереди
+          <p> 0/14 </p>
+        </div>
+      </div>
+    </div>
+</template>
+
+<script>
+
+export default {
+  name: 'Game'
+}
+</script>
+
+<style scoped lang="scss">
+  .game {
+    width: 100%;
+    height: 100vh;
+
+    &__sidebar {
+      display: flex;
+      flex-direction: column;
+      justify-content: space-between;
+      width: 32.5%;
+      height: 100%;
+      box-sizing: border-box;
+      background: $base-gradient;
+    }
+
+    &__menu {
+      display: flex;
+      align-items: center;
+      justify-content: space-between;
+      width: 50%;
+      margin-right:auto;
+      padding: 60px 60px 0 60px;
+    }
+
+    &__menu-item {
+      padding: 15px;
+      background-color: $c-white;
+      border-radius: 50%;
+    }
+
+    &__params {
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      flex-wrap: wrap;
+      padding: 60px 60px 0 60px;
+    }
+
+    &__params-item {
+      width: 48%;
+      padding: 10px;
+      display: flex;
+      align-items: center;
+      justify-content: space-between;
+      max-height: 190px;
+      margin-bottom:20px;
+      font-size: 48px;
+      line-height: 59px;
+      background: $c-white;
+      box-shadow: 0 0 50px rgba(131, 42, 64, 0.4);
+      box-sizing: border-box;
+      border-radius: 40px;
+    }
+
+    &__params-title {
+      width: 100%;
+      font-size: 36px;
+      line-height: 44px;
+      color: $c-white;
+      text-align: left;
+    }
+
+    .param__count {
+      margin-right: 30px;
+    }
+
+    &__queue {
+      height: 134px;
+      background: $c-white;
+      opacity: 0.15;
+      padding: 60px 60px 0 60px;
+    }
+  }
+</style>
---
+++ inchkiev/src/views/Home.vue
@@ -0,0 +1,79 @@
+<template>
+  <div class="home">
+    <div class="home__overlay"></div>
+    <div class="home__gradient">
+      <div class="home__content">
+        <p class="home__desc"> Добро пожаловать в игру </p>
+        <h1 class="home__title"> Помоги <br> ФАРМАЦЕВТУ </h1>
+        <div class="text-center">
+          <button class="home__button"> <router-link to="/game"> Hачать </router-link> </button>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+export default {
+  name: 'home'
+}
+</script>
+
+<style lang="scss">
+  .home {
+    width: 100%;
+    height: 100vh;
+    background-image: url('../assets/homebg.jpg');
+    background-size: cover;
+    color: $c-white;
+
+    &__overlay {
+      position: absolute;
+      width: 100%;
+      height: 100%;
+      background: $base-gradient;
+      opacity: 0.35;
+    }
+
+    &__gradient {
+      position: relative;
+      top: -40px;
+      left: 110px;
+      z-index: 111;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      width: 60%;
+      height: 83.9vh;
+      max-width: 860px;
+      max-height: 860px;
+      background: $base-gradient;
+      border-radius: 50%;
+    }
+
+    &__content {
+      text-align: left;
+    }
+
+    &__desc {
+      margin-bottom: 20px;
+      font-size: 35px;
+      line-height: 41px;
+      letter-spacing: 0.02em;
+      text-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+    }
+
+    &__title {
+      margin-top: 0;
+      margin-bottom: 11vh;
+    }
+
+    &__button {
+      padding: 20px 120px;
+      color: $c-blue-light;
+      background-color: $c-white;
+    }
+  }
+</style>
